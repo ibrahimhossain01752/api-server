a696e1cb66bea99d8fd2e75e91385de3da806206
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-11-18 21:16:02 +0600

    last added

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ require('dotenv').config();
 const ObjectId = require("mongodb").ObjectId;
 const cors = require('cors');
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.638jm.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://api_server:zEh5BWUp4syP50KA@cluster0.638jm.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 const port = 5000;
 const app = express();
@@ -15,31 +17,45 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-client.connect((err) => {
-    
-    const usersCollection = client.db("api").collection("users");
 
-    app.get("/users", async (req, res) => {
+async function run(){
+    try{
+        await client.connect();
+       
+       const database = client.db('api');
+   
+       const usersCollection = database.collection('users');
+       const bookingCollection = database.collection('bookingCollection');
+      
+       app.use('/users', async (req, res) => {
+        console.log(req.body);
         const result = await usersCollection.find({}).toArray();
+        console.log(result);
         res.send(result);
-      });
-    
-    
-      // single service........................
-     app.get("/users/:id", async (req, res) => {
-      const result = await usersCollection.find({_id: ObjectId(req.params.id)}).toArray();
-      res.send(result);
-     });
+    })
+
+    app.post('/Addusers', async (req, res) => {
+        console.log(req.body);
+        const result = await bookingCollection.insertOne(req.body);
+        res.send(result);
+    })
+
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir); 
+
+app.get('/', (req, res) => {
+        res.send('running beauti server');
+ })
+
+app.listen(port,() => {
+    console.log('running the server',port);
+ })
  
-
-});
-
-app.listen(process.env.PORT || port); 
-
 
 
 
